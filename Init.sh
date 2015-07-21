4e0e6887978c2cfc08640d2982c7ba4152cce89b
robin <robin.guignardperret@gmail.com>
2015-07-21 20:12:56 +0200

    [Init.sh now osx friendly]

--- Init.sh
+++ Init.sh
@@ -1,10 +1,25 @@
-red='\033[1;31m'
-green='\033[1;32m'
-NC='\033[0m' # No Color
-echo  "${red}WARNING: data-base have been freed !\n\
+#!/bin/bash
+
+red='\e[31m'
+yellow='\e[33m'
+green='\e[32m'
+NC='' # No Color
+echo -e "${yellow}WARNING: data-base have been freed !\n\
 - Data-base backup available (memorie_dump/rescue.bin)${NC}"
 wgdb export -f memorie_dump/rescue.bin
-echo  "${green}- Shared memorie allocated${NC}"
-sudo sysctl kernel.shmmax=1000000000
+
+if [ "$(uname)" == "Darwin" ]; then
+    # Do something under Mac OS X platform        
+	sudo sysctl -w kern.sysv.shmmax=1073741824
+	sudo sysctl -w kern.sysv.shmall=262144
+	echo -e "${green}- Shared memorie allocated(OSX)${NC}"
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    # Do something under Linux platform
+	sudo sysctl kernel.shmmax=1000000000
+	echo -e "${green}- Shared memorie allocated(linux)${NC}"
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    # Do something under Windows NT platform
+	echo -e "${red}[ERROR]No windows support !${NC}"
+fi
 wgdb aeonDB free
 wgdb create -l
